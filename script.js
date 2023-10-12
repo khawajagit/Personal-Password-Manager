49ab0972a00fe9db47fcfac626e80b5165cf384a
Laiq Ahmad Khawaja <129961221+khawajagit@users.noreply.github.com>
2023-10-11 22:48:45 -0700

    Personal Password Manager 1.0

---
+++ script.js
@@ -0,0 +1,93 @@
+let data = localStorage.getItem("passwords");
+
+
+
+const deletePassword = (website) => {
+    // let data = localStorage.getItem("passwords");
+    showPasswords();
+    let arr = JSON.parse(data);
+    arrUpdated = arr.filter((e) => {
+        return e.website != website;
+    })
+    localStorage.setItem("passwords", JSON.stringify(arrUpdated));
+    alert(`Successfully deleted ${website}'s password`);
+    showPasswords();
+};
+
+const showPasswords = () => {
+    let tb = document.querySelector("table");
+    if (data == null || JSON.parse(data).length == 0) {
+        tb.innerHTML = "No data to show";
+        tb.classList.add('red');
+    }
+    else {
+        tb.innerHTML = `<tr>
+        <th>Website</th>
+        <th>Username</th>
+        <th>Password</th>
+        <th>Actions</th>
+    </tr>`
+        let arr = JSON.parse(data);
+        let str = "";
+        for (let index = 0; index < arr.length; index++) {
+            const element = arr[index];
+            str += `<tr>
+                <td>${element.website}</td>
+                <td>${element.username} <i class="fa-solid fa-copy" onclick="copyText('${element.username}')"></i> </td>
+                <td>${maskPassword(element.password)} <i class="fa-solid fa-copy" onclick="copyText('${element.password}')"></i> </td>
+                <td><button class="btn" onclick="deletePassword('${element.website}')">Delete</button></td>
+            </tr>`;
+        }
+        tb.innerHTML = tb.innerHTML + str;
+
+        website.value = "";
+        username.value = "";
+        password.value = "";
+    }
+
+};
+
+function maskPassword(pass) {
+    let str = "";
+    for (let index = 0; index < pass.length; index++) {
+        str += "*";
+    }
+    return str;
+}
+
+function copyText(txt) {
+    navigator.clipboard.writeText(txt).then(
+        () => {
+            let span = document.querySelector('span');
+            span.classList.add('show');
+            setTimeout(() => {
+                span.classList.remove('show');
+            }, 2000)
+        },
+        () => {
+            span.innerHTML = "(Not Copied!)";
+        }
+    );
+}
+console.log("Working")
+showPasswords();
+document.querySelector(".submit").addEventListener("click", (e) => {
+    document.addEventListener('contextmenu', event => event.preventDefault());
+    e.preventDefault();
+    // console.log("clicked....");
+    let passwords = localStorage.getItem("passwords")
+    // console.log(passwords);
+    if (passwords == null) {
+        let json = []
+        json.push({ website: website.value, username: username.value, password: password.value })
+        alert("Password Saved");
+        localStorage.setItem("passwords", JSON.stringify(json));
+    }
+    else {
+        let json = JSON.parse(localStorage.getItem("passwords"));
+        json.push({ website: website.value, username: username.value, password: password.value })
+        alert("Password Saved");
+        localStorage.setItem("passwords", JSON.stringify(json));
+        showPasswords();
+    }
+});
